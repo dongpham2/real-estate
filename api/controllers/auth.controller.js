4e9fe2cdca4cf1aa38f0c2c2c40722070e519aa4
dongpham2 <dongpham21082001@gmail.com>
2024-03-06 10:45:13 +0700

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+import bcryptjs from "bcryptjs";
+import User from "../model/user.modules.js";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hardPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hardPassword,
+  });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
