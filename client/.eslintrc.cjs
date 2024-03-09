da2be2b736653a8e642175888a732195455c55e9
dongpham2 <dongpham21082001@gmail.com>
2024-03-09 10:07:32 +0700

    config Redux Toolkit

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -2,17 +2,28 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "react-app",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": [
+      "warn",
+      {
+        allowConstantExport: true,
+        arrowParens: "always",
+        semi: false,
+        trailingComma: 2,
+        endOfLine: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true,
+      },
     ],
   },
-}
+};
